500e94bd5fb9d1258dbe5e7272eab0c25d9c6c86
George Rae <george_rae06@hotmail.co.uk>
2022-08-23 23:53:37 +0100

    fix: remove tacky circle

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -18,7 +18,7 @@ const types = (await details.value).types;
 <template>
   <li class="list-item" :class="'list-item--' + types[0].type.name" >
     <h2 class="list-item__name">{{ name }}</h2>
-    <h3 class="list-item__id">{{ entry }}</h3>
+    <h3 class="list-item__id">#{{ entry }}</h3>
     <img :src="sprite" />
     <div class="list-item__types">
       <p v-for="type in types" :key="type">
