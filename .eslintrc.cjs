c446227b8b224b464f1d981aa0362523b256ceeb
George Rae <george.rae@28b.co.uk>
2022-10-17 16:09:33 +0100

    ci: tidy up linting and formatting issues.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,21 @@ module.exports = {
   root: true,
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
+    "@vue/airbnb",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
   ],
   parserOptions: {
     ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    camelcase: 0,
+    "no-use-before-define": 0,
+    "no-nested-ternary": 0,
+    "no-prototype-builtins": 0,
+    "@typescript-eslint/ban-types": 0,
+    "no-param-reassign": 0,
+    "vue/script-setup-uses-vars": 0,
   },
 };
